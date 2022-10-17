b7e99896e632a31707414cad73aecb716bffd122
iamsven2005 <108319539+iamsven2005@users.noreply.github.com>
2022-10-17 21:08:25 +0800

    maybach

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,17 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+
     <div className={styles.container}>
       <Head>
         <title>EDUHOPE</title>
         <link rel="icon" href="/EDUHOPE.png" />
       </Head>
-
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to EDUHOPE See <Link href="/SUBJECTS">SUBJECTS</Link>
@@ -20,7 +25,7 @@ export default function Home() {
           About Us:
         </h3>
         <p>
-          
+
         </p>
         <iframe width="1280" height="720" src="https://iamsven2005.github.io/horizontal/test2.html"  frameborder="0"  allowfullscreen></iframe>
         <iframe width="1280" height="720" src="https://my.spline.design/superkidrobotcopy-d9113a7280fd40fdc29a15abe4be23d4/"  frameborder="0"  allowfullscreen></iframe>

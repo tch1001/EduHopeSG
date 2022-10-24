ddd47749e8385b6fa5da1fd059d6e78a111db9f6
iamsven2005 <108319539+iamsven2005@users.noreply.github.com>
2022-10-24 10:39:45 +0800

    nav change

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -10,7 +10,8 @@ export default function Home() {
       <title>EDUHOPE</title>
         <link rel="icon" href="/EDUHOPE.png" />
 </Head>
-      <div className={styles.profile}><Link href="/"><a className={styles.profc}>BACK</a></Link><Link href="/tuteefaq"><a className={styles.otherprof}>Tutee FAQ</a></Link><Link href="/tutorfaq"><a className={styles.otherprof}>Tutor FAQ</a></Link><Link href="/"><a className={styles.otherprof}>SUBJECTS</a></Link><Link href="/aboutus"><a className={styles.otherprof}>ABOUT US</a></Link></div>
+
+      <div className={styles.profile}><Link href="/"><a className={styles.profc}>BACK</a></Link><Link href="/FAQ"><a className={styles.otherprof}>FAQ</a></Link><Link href="/CONTACTUS"><a className={styles.otherprof}>CONTACT US</a></Link><Link href="/SUBJECTS"><a className={styles.otherprof}>SUBJECTS</a></Link><Link href="/aboutus"><a className={styles.otherprof}>ABOUT US</a></Link></div>
       <section>
       <div className={styles.titleau}>
         <h3>EDUHOPE</h3><br></br><br></br>
@@ -53,17 +54,32 @@ export default function Home() {
     </section>
     
     <section>
-      <div className={styles.block}>
+      <div className={styles.blockau}>
         <p><span className={styles.firstau}>E</span>DUHOPE is a team of volunteers dedicated towards improving the learning experience. Through EDUHOPE, resources can be shared within and across student communities, enabling users to access and build upon a peer-to-peer learning environment. We believe that by facilitating such communal, generational learning, we can help close the loop between students and alumni, creating a learning cycle that grows with each new class of students, rather than simply repeating itself each year.</p>
         <p className={styles.linebreakau}></p>
-        <p className={styles.marginau}>Details:
+        <p className={styles.marginau}>
+        <h3>Tutor benefits – </h3>joy from helping and giving back!<br></br>
+        Details:
 
             Open to all secondary school (taking O Lvls) and JC students (taking A Lvls)<br></br>
             Will last for one month (around 4 sessions). Volunteers can sign up for a longer duration, but tutees will have to “renew” their subscription<br></br>
             1-1 sessions<br></br>
             Virtual consults</p>
+            <p className={styles.linebreakau}></p>
+          <h3>Tutee benefits – </h3>free and flexible consultations!<br></br>
+        <ul>
+          <li>Form meaningful relationships!</li>
+          <li>Seek advice outside of academics!</li>
+          <li>Learn from the young veterans (they have A for A levels and A1 for O level subjects)!</li>
+        </ul>
+        <p>Why should you volunteer?
+
+Do <em>YOU</em> believe in our vision? Do <em>YOU</em> want to help people without spending so much time preparing materials? Then this just the thing for <em>YOU!</em><br></br>We believe in the power of free peer-to-peer consults in creating a more affordable option for students.</p>
       </div>
+      
     </section>
+    <p className={styles.linebreakau}></p>
+    <h1 className={styles.hau}>“The most selfish thing you can do in this world is help someone else, because the gratification, the goodness that comes to you, the good feeling, the good feeling from helping others—nothing is better than that.” </h1><h3>-Denzel Washington, Academy Award Actor</h3>
     </body>
     
   )

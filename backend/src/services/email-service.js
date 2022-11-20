fce2307e5fd61416273332e0e984af37debb05da
Felix Isaac <me@felixisaac.dev>
2022-11-21 03:36:04 +0800

    useless things might delete in the future

---
+++ backend/src/services/email-service.js
@@ -0,0 +1,5 @@
+/**
+ * 1. Tutor receives an email request from the system with the option to decline or accept
+ * 2. Tutors receive an email about this change
+ * 3. Email tutee/tutor for testimonial
+ */

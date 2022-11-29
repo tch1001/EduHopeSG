20b9aa33625db1571fc03d42411310955f963f65
Felix Isaac <me@felixisaac.dev>
2022-11-29 22:02:27 +0800

    add(test): tutee service

--- backend/src/utils/sql-parser.js
+++ backend/src/utils/sql-parser.js
@@ -4,6 +4,15 @@ export default function parseSQL(query, values) {
     const hydratedConditions = [];
 
     const operation = query.split(" ")[0].trim();
+
+    // only handling select operation
+    if (operation !== "SELECT") return {
+        operation,
+        table: undefined,
+        conditions: undefined,
+        fields: undefined
+    };
+
     const table = query.toUpperCase().split(" FROM ")[1].split(" WHERE ")[0].toLowerCase();
     const fields = query.substring(operation.length, query.indexOf(table) - " FROM ".length);
     const conditions = query.substring(query.indexOf(table) + table.length + " WHERE ".length);
---
+++ backend/tests/tutee-service.spec.js
@@ -0,0 +1,170 @@
+import sinon from "sinon";
+import { expect } from "chai";
+import log from "../src/utils/logging.js";
+import pool from "../src/utils/database.js";
+import parseSQL from "../src/utils/sql-parser.js"
+import ServiceError from "../src/classes/ServiceError.js";
+import * as TuteeService from "../src/services/tutee-service.js";
+
+describe("Testing tutee service", () => {
+    const fakeTutee = {
+        id: "e99bf9b122caaac6eb9b7ef4b438bf5e83eb2e1d3e",
+        name: "Fake Tutee",
+        email: "tutee@encrypted.com",
+        password: "H@SH3D_p@ssw0rd as tutee",
+        school: "Fake Secondary School",
+        level_of_education: "Secondary 3",
+        telegram: "my_tutee",
+        bio: "This is a test tutee therefore, I'm faked",
+        referral: "Reddit"
+    }
+
+    const fakeTutor = {
+        id: "e99bf9b122caaac6eb9b7ef4b438bf5e83eb2e1d4e",
+        name: "Fake Tutor",
+        email: "tutor@encrypted.com",
+        password: "H@SH3D_p@ssw0rd as tutor",
+        school: "Fake Collage",
+        level_of_education: "JC 2",
+        telegram: "my_tutor",
+        bio: "This is a test tutor therefore, I'm faked",
+        referral: "Discord",
+        is_tutor: true,
+        tutoring: ['N', 'O'],
+        subjects: [1, 2, 3],
+        tutee_limit: 3,
+        commitment_end: new Date(Date.now() + (60 ** 2) * 24 * 30.5 * 10 * 1000) // 10 months
+    }
+
+    const validSubjects = fakeTutor.subjects;
+    const invalidSubjects = [...fakeTutor.subjects, 10];
+
+    before(() => {
+        function fakeQuery(query, values) {
+
+            if (!values) {
+                values = query?.values;
+                query = query?.text;
+            }
+
+            if (!query || !values) return { rows: [] };
+
+            const eduhope_users = [fakeTutee, fakeTutor];
+            const parsed = parseSQL(query, values);
+
+            // fake DB lol
+            if (parsed.operation === "SELECT" && parsed.table === "eduhope_user") {
+                const result = eduhope_users.find(user => {
+                    const conditionsMet = parsed.conditions.every(condition => {
+                        const [field, value] = condition.split(" = ");
+                        return user[field] === value;
+                    })
+
+                    return conditionsMet;
+                })
+
+                return { rows: [result] }
+            }
+            if (parsed.operation === "SELECT" && parsed.table === "tutee_tutor_relationship") {
+                if (parsed.fields[0].startsWith("count")) return { rows: [{ count: '0' }] };
+            }
+
+            return { rows: [] }
+        }
+
+        const fakeClient = { query: fakeQuery, release: () => { } };
+
+        sinon.stub(log);
+        sinon.stub(pool, "connect").resolves(fakeClient);
+    })
+
+    after(async () => {
+        sinon.restore();
+    })
+
+    describe("Request tutor", () => {
+        it("should not request due to missing parameters", async () => {
+            try {
+                const res = await TuteeService.requestTutor();
+                expect(res).to.be.undefined();
+            } catch (err) {
+                const expectedError = new ServiceError("user-request-tutor-missing");
+
+                expect(err.code).to.equal(expectedError.code);
+                expect(err.details).to.equal(expectedError.details);
+                expect(err.message).to.equal(expectedError.message);
+                expect(err.status).to.equal(expectedError.status);
+            }
+        })
+
+        it("should not request due to missing subjects", async () => {
+            try {
+                const res = await TuteeService.requestTutor(fakeTutee.id, fakeTutee.id, []);
+                expect(res).to.be.undefined();
+            } catch (err) {
+                const expectedError = new ServiceError("user-request-tutor-missing");
+
+                expect(err.code).to.equal(expectedError.code);
+                expect(err.details).to.equal(expectedError.details);
+                expect(err.message).to.equal(expectedError.message);
+                expect(err.status).to.equal(expectedError.status);
+            }
+        })
+
+        it("should not request due same user", async () => {
+            try {
+                const res = await TuteeService.requestTutor(fakeTutee.id, fakeTutee.id, validSubjects);
+                expect(res).to.be.undefined();
+            } catch (err) {
+                const expectedError = new ServiceError("tutee-tutor-same");
+
+                expect(err.code).to.equal(expectedError.code);
+                expect(err.details).to.equal(expectedError.details);
+                expect(err.message).to.equal(expectedError.message);
+                expect(err.status).to.equal(expectedError.status);
+            }
+        })
+
+        it("should not request due to tutee not found", async () => {
+            try {
+                const res = await TuteeService.requestTutor("not found", fakeTutor.id, validSubjects);
+                expect(res).to.be.undefined();
+            } catch (err) {
+                const expectedError = new ServiceError("tutee-tutor-not-found");
+
+                expect(err.code).to.equal(expectedError.code);
+                expect(err.details).to.equal(expectedError.details);
+                expect(err.message).to.equal(expectedError.message);
+                expect(err.status).to.equal(expectedError.status);
+            }
+        })
+
+        it("should not request due to tutor not found", async () => {
+            try {
+                const res = await TuteeService.requestTutor(fakeTutee.id, "not found", validSubjects);
+                expect(res).to.be.undefined();
+            } catch (err) {
+                const expectedError = new ServiceError("tutee-tutor-not-found");
+
+                expect(err.code).to.equal(expectedError.code);
+                expect(err.details).to.equal(expectedError.details);
+                expect(err.message).to.equal(expectedError.message);
+                expect(err.status).to.equal(expectedError.status);
+            }
+        })
+
+        it("should not request due to user not being a tutor", async () => {
+            try {
+                const res = await TuteeService.requestTutor(fakeTutor.id, fakeTutee.id, validSubjects);
+                expect(res).to.be.undefined();
+            } catch (err) {
+                const expectedError = new ServiceError("user-not-tutor");
+
+                expect(err.code).to.equal(expectedError.code);
+                expect(err.details).to.equal(expectedError.details);
+                expect(err.message).to.equal(expectedError.message);
+                expect(err.status).to.equal(expectedError.status);
+            }
+        })
+
+});

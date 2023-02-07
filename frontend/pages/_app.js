e6aa2f2406c21f197c1530fc15b7adde938678ba
Felix Isaac <me@felixisaac.dev>
2023-02-08 00:32:30 +0800

    add: dependencies

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,13 @@
+import Head from "../src/components/head.jsx"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <body>
+      <Head />
+      <Component {...pageProps} />
+    </body>
+  )
 }
 
 export default MyApp
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,17 +1,11 @@
-import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return (
         <div>
-            <Head>
-                <title>EduHope</title>
-                <meta name="description" content="Empowering students through free and flexible tutoring. — Find your volunteer tutor today!" />
-                <link rel="icon" href="/favicon.ico" />
-                
-            </Head>
-        </div>    
+            <p>meow</p>
+        </div>
     )
 }
 

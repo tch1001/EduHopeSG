63bd924ae1f0a0d2a3c043bbaf8429a56386800b
Felix Isaac <me@felixisaac.dev>
2023-02-13 04:33:28 +0800

    feat(landing page): subject cards

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,43 +1,11 @@
 import Image from "next/image";
-import { Container } from "../components/container";
+import { Button } from "../components/Button";
+import { Container } from "../components/Container";
+import { SubjectCard } from "../components/SubjectCard";
+import { BenefitCard } from "../components/home/BenefitCard";
 import { useEffect, useState } from "react";
 
-const BenefitCard = ({ illustration, tagline, description, alternate = false }) => {
-    const flexDirection = "flex-col md:flex-row";
-    const padding = "sm:w-9/12 p-8 sm:px-16 sm:py-12";
-    const colorStyling =  alternate ? "border-dark-aqua bg-aqua" : "border-gray-200 bg-gray-50";
-
-    let compoundedClassName = `flex gap-12 ${flexDirection} ${padding} border ${colorStyling} rounded-xl items-center`;
-
-
-    return (
-        <div className={compoundedClassName}>
-            {
-                alternate ?
-                    (
-                        <>
-                            <Image className="w-fit md:w-1/2" src={illustration} alt="" width={300} height={300} />
-                            <div className="text-black">
-                                <p className="uppercase text-2xl font-medium">{tagline}</p>
-                                <p className="text-base">{description}</p>
-                            </div>
-                        </>
-                    ) :
-                    (
-                        <>
-                            <div className="text-black">
-                                <p className="uppercase text-2xl font-medium">{tagline}</p>
-                                <p className="text-base">{description}</p>
-                            </div>
-                            <Image className="w-fit md:w-1/2" src={illustration} alt="" width={300} height={300} />
-                        </>
-                    )
-            }
-        </div>
-    )
-}
-
-export default function Home() {
+export default function Home({ subjects }) {
     const [width, setWidth] = useState(0);
 
     useEffect(() => {
@@ -64,31 +32,71 @@ export default function Home() {
                     <p>Empowering students through free and flexible tutoring.</p>
                 </div>
             </div>
-            <Container center className="py-12 gap-9">
-                <BenefitCard
-                    illustration="/images/landing_page/study_anywhere.png"
-                    tagline="Free and flexible consultations"
-                    description="Our volunteer tutors are passionate graduates who want you to succeed in your student life!"
-                />
-                <BenefitCard
-                    alternate
-                    illustration="/images/landing_page/5_stars.png"
-                    tagline="Quality Control"
-                    description="Every tutor is reviewed and vetted by our team to ensure you get the very best."
-                />
-                <BenefitCard
-                    illustration="/images/landing_page/study_anywhere.png"
-                    tagline="Build Connections, Enhance Your Learning."
-                    description="Find your perfect study partner and form meaningful relationships with experienced tutors."
-                />
-                <BenefitCard
-                    alternate
-                    illustration="/images/landing_page/5_stars.png"
-                    tagline="Expand Your Horizons"
-                    description="Get advice and guidance beyond academics and enhance your overall learning experience."
-                />
+            <Container>
+                <div className="flex flex-col items-center my-12 gap-9">
+                    <BenefitCard
+                        illustration="/images/landing_page/study_anywhere.png"
+                        tagline="Free and flexible consultations"
+                        description="Our volunteer tutors are passionate graduates who want you to succeed in your student life!"
+                    />
+                    <BenefitCard
+                        alternate
+                        illustration="/images/landing_page/5_stars.png"
+                        tagline="Quality Control"
+                        description="Every tutor is reviewed and vetted by our team to ensure you get the very best."
+                    />
+                    <BenefitCard
+                        illustration="/images/landing_page/study_anywhere.png"
+                        tagline="Build Connections, Enhance Your Learning."
+                        description="Find your perfect study partner and form meaningful relationships with experienced tutors."
+                    />
+                    <BenefitCard
+                        alternate
+                        illustration="/images/landing_page/5_stars.png"
+                        tagline="Expand Your Horizons"
+                        description="Get advice and guidance beyond academics and enhance your overall learning experience."
+                    />
+                    <BenefitCard
+                        illustration="/images/landing_page/study_anywhere.png"
+                        tagline="Learn from the Best, Reach Your Potential."
+                        description="Get personalized attention from young veterans who have excelled in their studies and reach your full potential."
+                    />
+                </div>
+                <div className="flex flex-col items-center my-12 gap-9">
+                    <p className="text-2xl font-semibold">Subjects available</p>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-6">
+                        {
+                            subjects.map((subject, key) => (
+                                <SubjectCard
+                                    key={key}
+                                    image={"/images/landing_page/subject.jpg"}
+                                    name={subject.name}
+                                    stream={subject.course}
+                                    tutors={subject.tutor_count}
+                                />
+                            ))
+                        }
+                    </div>
+                    <Button>Explore more subjects</Button>
+                </div>
             </Container>
         </div>
     )
 }
 
+export const getStaticProps = async () => {
+    // TODO: Request from back end server using fetch()
+    // JSON file is used as a placeholder for development
+    // purposes and will not be used in production!
+    const file = await import("../data/subjects.json");
+    const subjects = Object.values(JSON.parse(JSON.stringify(file)));
+
+    // only need 9 for landing
+    subjects.splice(9);
+
+    return {
+        props: {
+            subjects
+        }
+    }
+} 

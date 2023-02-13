8568e095d6d9d6885433072d1102ff2e181083d1
Felix Isaac <me@felixisaac.dev>
2023-02-13 16:38:26 +0800

    fix(date): use dayjs instead of formatted date string

--- frontend/components/home/TestimonialCard.jsx
+++ frontend/components/home/TestimonialCard.jsx
@@ -1,4 +1,9 @@
-import { Card } from "../Card"
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { Card } from "../Card";
+
+import "dayjs/locale/en-sg";
+dayjs.extend(relativeTime);
 
 export const TestimonialCard = ({
     testimonial,
@@ -9,12 +14,14 @@ export const TestimonialCard = ({
     tutor = true,
     ...props
 }) => {
+
+
     return (
         <Card className="pt-2 pb-4 px-6 min-w-[16rem] xs:min-w-[22rem] sm:min-w-[28rem] snap-start" {...props}>
             <p className="font-medium text-base">"{testimonial}"</p>
             <div className="text-sm mt-3">
-                <p className="text-dark-blue">— {name}. {tutor ? "Tutor" : "Tutee"} since {member_since}</p>
-                <p className="font-medium">{stream} {tutor ? "tutor" : "tutee"}. Studying in {institution}</p>
+                <p className="text-dark-blue">— {name}. {tutor ? "Tutor" : "Tutee"} since {dayjs().to(member_since)}</p>
+                <p className="font-medium">{stream} {tutor ? "tutor" : "student"}. Studying in {institution}</p>
             </div>
         </Card>
     )

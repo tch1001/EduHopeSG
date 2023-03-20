90509737976b672d4d97f16fa3b9ce2e6cfd04a3
Felix Isaac <38658663+FelixIsaac@users.noreply.github.com>
2023-03-20 17:39:48 +0900

    feat(login): add form validation

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import Link from "next/link";
 import Button from "../components/Button";
 import Container from "../components/Container";
@@ -6,64 +7,62 @@ import Card from "../components/Card";
 import styles from "../styles/login.module.css";
 
 function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const LoginSchema = Yup.object({
+        email: Yup.string()
+            .email("Invalid email address")
+            .required("Required"),
+        password: Yup.string().required("Required")
+    });
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: ""
+        },
+        validationSchema: LoginSchema,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    });
+
+    function FormErrorDisplay({ field }) {
+        if (!formik.touched[field] || !formik.errors[field]) return;
+
+        return (
+            <div className=" text-red-500 font-extralight text-xs">
+                {formik.errors[field]}
+            </div>
+        )
     }
-
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (!email || !password) {
-            alert("Please enter both an email and a password.");
-            return;
-        }
-
-        // Check if the email is valid using a regular expression
-        const emailRegex = /^\S+@\S+\.\S+$/;
-        if (!emailRegex.test(email)) {
-            alert("Please enter a valid email address.");
-            return;
-        }
-
-        // Check if the password is at least 6 characters long
-        if (password.length < 6) {
-            alert("Please enter a password that is at least 6 characters long.");
-            return;
-        }
-
-        // If input is valid, you can submit the form data to a server for authentication.
-    };
-
 
     return (
         <Container center className="p-6 max-w-5xl">
             <Card className="p-4 m-2">
                 <span className="text-2xl font-bold block text-center my-2">Login</span>
-                <form className="flex flex-col items-center gap-2" onSubmit={handleSubmit} noValidate autoComplete="off">
+                <form
+                    className="flex flex-col items-center gap-2"
+                    onSubmit={formik.handleSubmit}
+                    noValidate
+                    autoComplete="off"
+                >
                     <div className="w-full max-w-sm px-4 py-2">
+                        <FormErrorDisplay field="email"/>
                         <label htmlFor="email">Email address</label>
                         <input
                             type="email"
                             id="email"
                             className={styles.input}
-                            value={email}
-                            onChange={handleEmailChange}
+                            {...formik.getFieldProps("email")}
                         />
                     </div>
                     <div className="w-full max-w-sm px-4 py-2">
+                        <FormErrorDisplay field="password" />
                         <label htmlFor="password">Password</label>
                         <input
                             type="password"
                             id="password"
                             className={styles.input}
-                            value={password}
-                            onChange={handlePasswordChange}
+                            {...formik.getFieldProps("password")}
                         />
                     </div>
                     <Button type="submit">Login</Button>

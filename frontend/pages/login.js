3a06ba0646f51391a00fb378bf8bc60bd47b8259
Felix Isaac <38658663+FelixIsaac@users.noreply.github.com>
2023-03-20 15:33:46 +0900

    refactor: login page
    - Rearranged order of imported files as per best practices
    - Replaced single quotes (') to double quotes (")
    - Use TailwindCSS and removed styles from login.module.css
    - Used Card and Button components to reduce file size

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,69 +1,80 @@
-import { useState } from 'react';
+import { useState } from "react";
+import Link from "next/link";
+import Button from "../components/Button";
 import Container from "../components/Container";
-import Link from 'next/link';
+import Card from "../components/Card";
 import styles from "../styles/login.module.css";
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  }
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!email || !password) {
-      alert("Please enter both an email and a password.");
-      return;
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
     }
-  
-    // Check if the email is valid using a regular expression
-    const emailRegex = /^\S+@\S+\.\S+$/;
-    if (!emailRegex.test(email)) {
-      alert("Please enter a valid email address.");
-      return;
-    }
-  
-    // Check if the password is at least 6 characters long
-    if (password.length < 6) {
-      alert("Please enter a password that is at least 6 characters long.");
-      return;
-    }
-  
-    // If input is valid, you can submit the form data to a server for authentication.
-  };
-  
 
-  return (
-    <Container center className="p-6 max-w-5xl">
-      <div className={styles.container}>
-        <h1 className={styles.heading}>Login</h1>
-        <form onSubmit={handleSubmit} noValidate>
-          <div className={styles.outerContainer}>
-            <div className={styles.innerContainer}>
-              <label htmlFor="email" className={styles.label}>Email address</label>
-              <input type="email" className={styles.input} id="email" value={email} onChange={handleEmailChange} />
-            </div>
-            <div className={styles.innerContainer}>
-              <label htmlFor="password" className={styles.label}>Password</label>
-              <input type="password" className={styles.input} id="password" value={password} onChange={handlePasswordChange} />
-            </div>
-            <button type="submit" className={styles.button}>Login</button>
-          </div>
-        </form>
-      </div>
-      <p className={styles.text}>New to EduHope? 
-        <Link href="/signup" passHref className={styles.link}>
-          Join us Now
-        </Link>
-      </p>
-    </Container>
-  );
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!email || !password) {
+            alert("Please enter both an email and a password.");
+            return;
+        }
+
+        // Check if the email is valid using a regular expression
+        const emailRegex = /^\S+@\S+\.\S+$/;
+        if (!emailRegex.test(email)) {
+            alert("Please enter a valid email address.");
+            return;
+        }
+
+        // Check if the password is at least 6 characters long
+        if (password.length < 6) {
+            alert("Please enter a password that is at least 6 characters long.");
+            return;
+        }
+
+        // If input is valid, you can submit the form data to a server for authentication.
+    };
+
+
+    return (
+        <Container center className="p-6 max-w-5xl">
+            <Card className="p-4 m-2">
+                <span className="text-2xl font-bold block text-center my-2">Login</span>
+                <form className="flex flex-col items-center gap-2" onSubmit={handleSubmit} noValidate autoComplete="off">
+                    <div className="w-full max-w-sm px-4 py-2">
+                        <label htmlFor="email">Email address</label>
+                        <input
+                            type="email"
+                            id="email"
+                            className={styles.input}
+                            value={email}
+                            onChange={handleEmailChange}
+                        />
+                    </div>
+                    <div className="w-full max-w-sm px-4 py-2">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            className={styles.input}
+                            value={password}
+                            onChange={handlePasswordChange}
+                        />
+                    </div>
+                    <Button type="submit">Login</Button>
+                </form>
+            </Card>
+            <p>
+                New to EduHope?{" "}
+                <Link href="/signup" className="link" passHref>Join us Now</Link>
+            </p>
+        </Container>
+    );
 }
 
 export default Login;

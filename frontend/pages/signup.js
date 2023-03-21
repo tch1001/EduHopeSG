6e828d457c4634735209a60f74561d4538a6e142
Felix Isaac <38658663+FelixIsaac@users.noreply.github.com>
2023-03-21 23:12:58 +0900

    refactor: dynamic step changes

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
+import Link from "next/link";
+import Button from "../components/Button";
 import Container from "../components/Container";
+import Card from "../components/Card";
 import styles from "../styles/signup.module.css";
 
+/**
+ * Issues
+ * 1. No validation for difference of "password" and "confirmPassword"
+ * 2. 
+ */
+
 const SignUp = () => {
-  const [step, setStep] = useState(1);
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
@@ -12,14 +20,6 @@ const SignUp = () => {
   const [highestEducation, setHighestEducation] = useState('');
   const [currentEducation, setCurrentEducation] = useState('');
   const [schoolName, setSchoolName] = useState('');
-
-  const nextStep = () => {
-    setStep(step + 1);
-  };
-
-  const prevStep = () => {
-    setStep(step - 1);
-  };
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -35,96 +35,89 @@ const SignUp = () => {
     });
   };
 
-  switch(step) {
-    case 1:
-      return (
-        <Container center className="p-6 max-w-5xl">
-          <div className={styles.container}>
-            <h1 className={styles.heading}>Sign Up</h1>
-            <h2 className={styles.subHeading}>Step 1</h2>
-            <form onSubmit={nextStep} className={styles.form} noValidate>
-              <div className={styles.innerContainer}>
-                <label className={styles.label}>First Name:</label>
-                <input className={styles.input} value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-              </div>
-              <div className={styles.innerContainer}>
-                <label className={styles.label}>Last Name:</label>
-                <input className={styles.input} value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-              </div>
-              <div className={styles.innerContainer}>
-                <label className={styles.label}>Email Address:</label>
-                <input className={styles.input} value={email} onChange={(e) => setEmail(e.target.value)}/>
-              </div>
-              <div className={styles.buttonContainer}>
-                <button type="button" onClick={nextStep} className={styles.buttonRight}>Next</button>
-              </div>
-            </form>
-          </div>
-        </Container>
-      );
-    case 2:
-      return (
-        <Container center className="p-6 max-w-5xl">
-          <div className={styles.container}>
-            <h1 className={styles.heading}>Sign Up</h1>
-            <h2 className={styles.subHeading}>Step 2</h2>
-            <form onSubmit={nextStep} className={styles.form} noValidate>
-              <div className={styles.innerContainer}>
-                <label className={styles.label}>Password:</label>
-                <input className={styles.input} type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-              </div>
-              <div className={styles.innerContainer}>
-                <label className={styles.label}>Confirm Password:</label>
-                <input className={styles.input} type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
-              </div>
-              <div className={styles.buttonContainer}>
-                <button type="button" onClick={prevStep} className={styles.buttonLeft}>Back</button>
-                <button type="button" onClick={nextStep} className={styles.buttonRight}>Next</button>
-              </div>
-            </form>
-          </div>
-        </Container>
-      );
-    case 3:
-      return (
-        <Container center className="p-6 max-w-5xl">
-          <div className={styles.container}>
-            <h1 className={styles.heading}>Sign Up</h1>
-            <h2 className={styles.subHeading}>Step 3</h2>
-            <form onSubmit={onSubmit} className={styles.form} noValidate>
-              <div className={styles.innerContainer}>
-                <label className={styles.label}>Highest Education Level Obtained:</label>
-                <select className={styles.input} value={highestEducation} onChange={(e) => setHighestEducation(e.target.value)}>
-                  <option value="">--Select--</option>
-                  <option value="highSchool">High School</option>
-                  <option value="college">College</option>
-                  <option value="graduateSchool">Graduate School</option>
-                </select>
-              </div>
-              <div className={styles.innerContainer}>
-                <label className={styles.label}>Current Education:</label>
-                <select className={styles.input} value={currentEducation} onChange={(e) => setCurrentEducation(e.target.value)}>
-                  <option value="">--Select--</option>
-                  <option value="highSchool">High School</option>
-                  <option value="college">College</option>
-                  <option value="graduateSchool">Graduate School</option>
-                </select>
-              </div>
-              <div className={styles.innerContainer}>
-                <label className={styles.label}>Current School's Name:</label>
-                <input className={styles.input} value={schoolName} onChange={(e) => setSchoolName(e.target.value)}/>
-              </div>
-              <div className={styles.buttonContainer}>
-                <button type="button" onClick={prevStep} className={styles.buttonLeft}>Back</button>
-                <button type="submit" className={styles.buttonRight}>Submit</button>
-              </div>
-            </form>
-          </div>
-        </Container>
-      );
-    default:
-      return null;
+  const steps = [
+    <>
+      <div className={styles.innerContainer}>
+        <label className={styles.label}>First Name:</label>
+        <input className={styles.input} value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+      </div>
+      <div className={styles.innerContainer}>
+        <label className={styles.label}>Last Name:</label>
+        <input className={styles.input} value={lastName} onChange={(e) => setLastName(e.target.value)} />
+      </div>
+      <div className={styles.innerContainer}>
+        <label className={styles.label}>Email Address:</label>
+        <input className={styles.input} value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+    </>,
+    <>
+      <div className={styles.innerContainer}>
+        <label className={styles.label}>Password:</label>
+        <input className={styles.input} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div className={styles.innerContainer}>
+        <label className={styles.label}>Confirm Password:</label>
+        <input className={styles.input} type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+      </div>
+    </>,
+    <>
+      <div className={styles.innerContainer}>
+        <label className={styles.label}>Highest Education Level Obtained:</label>
+        <select className={styles.input} value={highestEducation} onChange={(e) => setHighestEducation(e.target.value)}>
+          <option value="">--Select--</option>
+          <option value="highSchool">High School</option>
+          <option value="college">College</option>
+          <option value="graduateSchool">Graduate School</option>
+        </select>
+      </div>
+      <div className={styles.innerContainer}>
+        <label className={styles.label}>Current Education:</label>
+        <select className={styles.input} value={currentEducation} onChange={(e) => setCurrentEducation(e.target.value)}>
+          <option value="">--Select--</option>
+          <option value="highSchool">High School</option>
+          <option value="college">College</option>
+          <option value="graduateSchool">Graduate School</option>
+        </select>
+      </div>
+      <div className={styles.innerContainer}>
+        <label className={styles.label}>Current School's Name:</label>
+        <input className={styles.input} value={schoolName} onChange={(e) => setSchoolName(e.target.value)} />
+      </div>
+    </>
+  ];
+
+  const [step, setStep] = useState(0);
+
+  const checkNextStep = () => step + 1 >= steps.length;
+  const checkPrevStep = () => step <= 0;
+
+  const nextStep = () => {
+    if (!checkNextStep) return;
+    setStep(step + 1)
   }
+
+  const prevStep = () => {
+    if (!checkPrevStep) return;
+    setStep(step - 1);
+  }
+
+  return (
+    <Container center className="p-6 max-w-5xl">
+      <div className={styles.container}>
+        <div className="my-2">
+          <span className="text-2xl font-bold block text-center">Sign Up</span>
+          <span className="text-base block text-center">Step {step + 1} of {steps.length}</span>
+        </div>
+        <form className={styles.form} noValidate onSubmit={(e) => e.preventDefault()}>
+          {steps[step]}
+          <div className="flex flex-row gap-2">
+            <Button disabled={checkPrevStep()} onClick={prevStep}>Back</Button>
+            <Button disabled={checkNextStep()} onClick={nextStep}>Next</Button>
+          </div>
+        </form>
+      </div>
+    </Container>
+  );
 };
 
 export default SignUp;

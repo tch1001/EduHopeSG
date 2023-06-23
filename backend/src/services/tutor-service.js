6d7412a0718468241015f1b078d1944513a68adc
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2023-06-23 22:16:59 +0800

    remove: uncommented email function call

--- backend/src/services/tutor-service.js
+++ backend/src/services/tutor-service.js
@@ -172,7 +172,7 @@ export async function acceptTutee(relationshipID) {
     const { tutee, tutor, subject } = rows[0];
 
     // notify tutee of acceptance
-    //await notifyTuteeAcceptance(tutee, tutor, subject);
+    await notifyTuteeAcceptance(tutee, tutor, subject);
 
     return {
         success: true,
@@ -196,7 +196,7 @@ export async function rejectTutee(relationshipID, reason) {
 
     // notify tutee of rejection
     await query(`DELETE ${queryText}`, [relationshipID]);
-    //await notifyTuteeDeclination(tutee, tutor, subject, reason);
+    await notifyTuteeDeclination(tutee, tutor, subject, reason);
 
     return {
         success: true,
@@ -222,7 +222,7 @@ export async function removeTutee(relationshipID, reason) {
 
     // notify tutee of removal
     await query(`DELETE ${queryText}`, [relationshipID]);
-    //await notifyTuteeDeclination(tutee, tutor, subjects, reason);
+    await notifyTuteeDeclination(tutee, tutor, subjects, reason);
 
     return {
         success: true,

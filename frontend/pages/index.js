dd52d7fd1161d6a9142be345199e4ad8abd4d078
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2023-07-31 19:12:30 +0800

    update(landing): minor redesign

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -83,7 +83,7 @@ const Home = ({ testimonials }) => {
 
     return (
         <div>
-            <div className="relative text-center text-white">
+            <div className="relative text-center">
                 <Image
                     src="/images/landing_page/cover_banner.jpg"
                     className="w-screen h-fit object-cover"
@@ -94,20 +94,22 @@ const Home = ({ testimonials }) => {
                     priority
                     alt=""
                 />
-                <div className={`${styles.cover} flex flex-col gap-1 m-auto text-white text-center text-3xl`}>
-                    <p className="uppercase font-bold">Connect, Learn and Grow</p>
-                    <p>To create a free alternative for students seeking supplementary education.</p>
+                <div className={`${styles.cover} flex flex-col gap-12 m-auto`}>
+                    <div className="flex flex-col gap-5 m-auto text-white text-center drop-shadow-[0_5px_5px_rgba(0,0,0,1)]">
+                        <p className="font-bold text-5xl">EduhopeSG</p>
+                        <p className="text-2xl">Creating a free alternative for students seeking supplementary education.</p>
+                    </div>
+                    <div className="flex justify-center space-x-10 md:space-x-15 lg:space-x-20 uppercase font-bold text-2xl mx-3">
+                        <Button href="/subjects">
+                            Find a Tutor!
+                        </Button>
+                        <Button href="/signup/tutor">
+                            Become a Tutor!
+                        </Button>
+                    </div>
                 </div>
             </div>
             <Container className="flex flex-col gap-16 my-16">
-                <div className="flex justify-center space-x-10 md:space-x-15 lg:space-x-20 uppercase font-bold text-2xl mx-3">
-                    <Button href="/subjects">
-                        Find a Tutor!
-                    </Button>
-                    <Button href="/signup/tutor">
-                        Become a Tutor!
-                    </Button>
-                </div>
                 <div className="flex flex-col gap-16">
                     <div className="flex flex-col items-center gap-9">
                         <p className="text-2xl font-semibold">

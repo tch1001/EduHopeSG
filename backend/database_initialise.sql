7ea286a6252682a64659998da1840ce182bc3c8c
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2023-08-05 14:55:21 +0800

    update(db): changed education types

--- backend/database_initialise.sql
+++ backend/database_initialise.sql
@@ -4,6 +4,8 @@ CREATE EXTENSION IF NOT EXISTS "pg_trgm"
 -- Enums
 DROP TYPE IF EXISTS LEVEL;
 CREATE TYPE LEVEL AS ENUM (
+    'Pure',
+    'Combined',
     'H1',
     'H2',
     'H3',
@@ -13,29 +15,22 @@ CREATE TYPE LEVEL AS ENUM (
 
 DROP TYPE IF EXISTS EDUCATION;
 CREATE TYPE EDUCATION AS ENUM (
-    'SEC_1',
-    'SEC_2',
-    'SEC_3',
-    'SEC_4',
-    'SEC_5',
-    'JC_1',
-    'JC_2',
-    'PRIVATE_O_LEVEL',
-    'PRIVATE_A_LEVEL',
-    'IP_1',
-    'IP_2',
-    'IP_3',
-    'IP_4',
-    'IP_5',
-    'IP_6',
-    'IB_1',
-    'IB_2',
-    'POLYTECHNIC_0',
-    'POLYTECHNIC_1',
-    'POLYTECHNIC_2',
-    'POLYTECHNIC_3',
-    'UNI_UNDERGRADUATE',
-    'UNI_GRADUATE'
+    'Sec 1',
+    'Sec 2',
+    'Sec 3',
+    'Sec 4',
+    'Sec 5',
+    'JC 1',
+    'JC 2',
+    'JC Graduate',    
+    'Polytechnic Year 1',
+    'Polytechnic Year 2',
+    'Polytechnic Year 3',
+    'Polytechnic Graduate',    
+    'Private O''Level',
+    'Private A''Level', 
+    'Uni Undergraduate',
+    'Uni Graduate'
 );
 
 DROP TYPE IF EXISTS COURSE_ENUM;

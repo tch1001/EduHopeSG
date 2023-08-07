1f00bab185f6704620ec156cff96fcc511ffa00b
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2023-08-07 12:59:41 +0800

    update(about): minor change

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -68,7 +68,7 @@ const About = ({ teamMembers }) => {
                                     <div className="pt-2 pb-4 px-6">
                                         <p className="font-medium text-lg text-dark-aqua">{member.name}</p>
                                         <p className="font-medium text-sm text-dark-blue">{member.role}</p>
-                                        <p>{member.bio}</p>
+                                        <p className="mt-2">{member.bio}</p>
                                     </div>
                                 </Card>
                             ))
